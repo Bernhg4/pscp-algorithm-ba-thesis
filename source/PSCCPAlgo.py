f37739291907326d37cf2551561a159cce4db27b
Bernhard <bernhard.siegl04@gmail.com>
2024-10-01 16:12:35 +0200

    Refactoring of different components

---
+++ source/PSCCPAlgo.py
@@ -0,0 +1,46 @@
+import json
+import sys
+
+from source.jsonIO.jsonRW import solution_to_json, instance_from_json, solution_from_json, load_json_file, \
+    write_json_file
+from source.solvers.solutionGenerators import random_solution, demands_first
+from source.validator.ownSolutionValidator import validate
+
+if __name__ == "__main__":
+
+    instance_file = '../data/PSCCP_Instance7.json'
+
+    if len(sys.argv) == 3:
+        instance_file = open(sys.argv[1])
+        solution_file = open(sys.argv[2])
+
+    #load the instance
+    toy_instance_data = load_json_file(instance_file)
+
+    #converting the json to the data structure
+    toy_instance = instance_from_json(toy_instance_data)
+
+    #printing the model like in the problem description
+    print(toy_instance)
+
+    random_sol = random_solution(toy_instance)
+    demand_sol = demands_first(toy_instance)
+    demand_old_sol = demands_first(toy_instance,True)
+    demand_old_prior_sol = demands_first(toy_instance,True, True)
+
+    write_json_file(solution_to_json(random_sol), instance_file.replace('.json','_random_sol.json'))
+    write_json_file(solution_to_json(demand_sol), instance_file.replace('.json','_demand_sol.json'))
+    write_json_file(solution_to_json(demand_old_sol), instance_file.replace('.json','_demand_old_sol.json'))
+    write_json_file(solution_to_json(demand_old_prior_sol), instance_file.replace('.json','_demand_old_prior_sol.json'))
+
+    print("Random Solution:")
+    validate(toy_instance,random_sol)
+
+    print("\nDemands first solution:")
+    validate(toy_instance, demand_sol)
+
+    print("\nDemands first with old color solution:")
+    validate(toy_instance, demand_old_sol)
+
+    print("\nDemands first with old color and prioritization solution:")
+    validate(toy_instance, demand_old_prior_sol)
--- source/validator/ownSolutionValidator.py
+++ source/validator/ownSolutionValidator.py
@@ -1,7 +1,11 @@
+import sys
+import json
+
+from source.jsonIO.jsonRW import instance_from_json, solution_from_json
+
 
 def validate(instance, solution):
     demand_violations = 0
-    time_violations = 0
     color_changes = 0
 
     last_color = instance.history_color
@@ -15,27 +19,33 @@ def validate(instance, solution):
                 #check if current carrier is the carrier of the demand
                 if instance.rounds[rnd_index].scheduled_carriers[col_index] == demand.carrier_type:
                     #if the color matches, one demand less
-                    if color == demand.color:
+                    if color == demand.color and (rnd_index+1) <= demand.due_date:
                         left_quantity -= 1
-                        #if color is right but round too late
-                        if (rnd_index+1) > demand.due_date and left_quantity > 0:
-                            time_violations += 1
         #if some demands not (fully) fullfilled
         if left_quantity > 0:
             print(f"Demand {dem_index+1} is not fulfilled by {left_quantity}")
         demand_violations += 1 if left_quantity > 0 else 0
 
     #loop through the colors to get color changes
-    for rnd_index, rnd in enumerate(solution.round_solutions):
-        for col_index, color in enumerate(rnd.selected_colors):
+    for rnd in solution.round_solutions:
+        for color in rnd.selected_colors:
             if color != last_color:
                 color_changes += 1
                 last_color = color
 
-    if (demand_violations+time_violations) == 0:
+    if demand_violations == 0:
         print("Solution is feasible!")
     else:
         print("Solution is infeasible!")
         print(f"Demand violations: {demand_violations}")
-        print(f"Time violations: {time_violations}")
     print(f"Color changes: {color_changes}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: <instance_file.json> <solution_file.json>")
+
+    # Opening JSON files
+    instance_file = open(sys.argv[1])
+    solution_file = open(sys.argv[2])
+
+    validate(instance_from_json(json.load(instance_file)), solution_from_json(json.load(solution_file)))
