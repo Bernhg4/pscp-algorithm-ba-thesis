a7bd23a2c1eb34a63e7a2f989bcdbe91bfbc1ef8
Bernhard <bernhard.siegl04@gmail.com>
2024-10-05 20:52:17 +0200

    Implementing reverse demand solution generator

--- source/solvers/solutionGenerators.py
+++ source/solvers/solutionGenerators.py
@@ -30,12 +30,6 @@ def demands_first(input_instance, use_old_color=False, priorize_old_color=False,
         for _ in range(demand.quantity):  # Repeat for the quantity
             all_demands.append((demand.carrier_type, demand.color, demand.due_date))  # Append each tuple
 
-    #if priorize_due_date:
-    #all_demands = sorted(all_demands, key=lambda tup: tup[2])
-
-    #nimmt liste von demands die am frühesten fertig sein müssen
-    # davon eventuell die lastcolor sonst random
-
     #loop over each carrier of each round
     temp_col = 1 if input_instance.history_color==0 else input_instance.history_color
     for round_item in input_instance.rounds:
@@ -54,7 +48,7 @@ def demands_first(input_instance, use_old_color=False, priorize_old_color=False,
                         if dem[1] == temp_col:
                             dem_del = dem
                     if dem_del == (0, 0, 0) and priorize_due_date:
-                        dem_del = filtered_demands[randint(0, filtered_demands.__len__() - 1)]
+                        dem_del = filtered_demands[randint(0, len(filtered_demands) - 1)]
                         temp_col = dem_del[1]
 
                 if dem_del == (0, 0, 0):
@@ -75,3 +69,54 @@ def demands_first(input_instance, use_old_color=False, priorize_old_color=False,
         round_solutions.append(round_solution)
 
     return PSCP_Solution([d for d in round_solutions])
+
+def demands_reverse(input_instance):
+    round_solutions = []
+    all_demands = []
+
+    # document all demands
+    for demand in input_instance.demands:
+        for _ in range(demand.quantity):  # Repeat for the quantity
+            all_demands.append((demand.carrier_type, demand.color, demand.due_date))  # Append each tuple
+    #all_demands = sorted(all_demands, key=lambda d: d[2],reverse=True)
+
+    # loop over each carrier of each round
+    temp_col = -1
+    round_number = len(input_instance.rounds)
+    for round_item in reversed(input_instance.rounds):
+        selected_colors = [-1 for _ in round_item.scheduled_carriers]
+        col_ind = len(selected_colors)-1
+        current_demands = [x for x in all_demands if x[2] >= round_number]
+
+        for current_carrier in reversed(round_item.scheduled_carriers):
+
+            dem_del = (0, 0, 0)
+            filtered_demands = [d for d in current_demands if d[0] == current_carrier]
+            if filtered_demands:
+                for dem in filtered_demands:
+                    if dem[1] == temp_col:
+                        dem_del = dem
+
+                if dem_del == (0, 0, 0):
+                    dem_del = filtered_demands[randint(0, len(filtered_demands) - 1)]
+                    temp_col = dem_del[1]
+
+            if dem_del != (0, 0, 0):
+                all_demands.remove(dem_del)
+                current_demands.remove(dem_del)
+
+            selected_colors[col_ind] = temp_col
+            col_ind -= 1
+
+        for ind in range(len(round_item.scheduled_carriers)):
+            if selected_colors[ind] == -1:
+                selected_colors[ind] = selected_colors[ind-1]
+        temp_col = selected_colors[0]
+
+
+
+        round_number -= 1
+        round_solution = RoundSolution(selected_colors)
+        round_solutions.append(round_solution)
+
+    return PSCP_Solution([d for d in reversed(round_solutions)])
