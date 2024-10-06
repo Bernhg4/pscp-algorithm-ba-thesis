45df5ce34219d9f02e40533a0824438fdda2178f
Bernhard <bernhard.siegl04@gmail.com>
2024-10-06 21:41:11 +0200

    Working on local switch

--- source/solvers/solutionImprovers.py
+++ source/solvers/solutionImprovers.py
@@ -1,3 +1,5 @@
+from source.models.baseModels import RoundSolution, PSCP_Solution
+
 
 def local_reorder(input_instance, solution):
 
@@ -16,9 +18,11 @@ def local_reorder(input_instance, solution):
             temp_colors.append(sel_color)
 
             if (idx+1) < max_idx:
+                if start_idx == -1:
+                    start_idx = idx
                 if rounds[round_idx].scheduled_carriers[idx+1] == rounds[round_idx].scheduled_carriers[idx]:
-                    start_idx = idx if start_idx==-1 else start_idx
                     continue
+
 
             for col in sorted(temp_colors):
                 solution.round_solutions[round_idx].selected_colors[start_idx] = col
@@ -31,10 +35,8 @@ def local_reorder(input_instance, solution):
 
 def local_switch(input_instance, solution):
 
-    #jetzt schauen ob color zuvor zu einer der von dem carrier passt, dann reordern
-    # gruppen erstellen pro carrier: ein array mit indizes je pro color, evnetuell tupel
-
     solution = local_reorder(input_instance, solution)
+    new_solutions = []
 
     rounds = input_instance.rounds
 
@@ -56,21 +58,69 @@ def local_switch(input_instance, solution):
         grp_idx = 1
         while (grp_idx+1) < len(groups):
 
-            grp_before = list(groups[grp_idx-1])
-            grp_now = list(groups[grp_idx])
-            grp_after = list(groups[grp_idx+1])
+            grp_before = list(groups[grp_idx-1][1])
+            grp_now = list(groups[grp_idx][1])
+            grp_after = list(groups[grp_idx+1][1])
 
             new_group_before = [-1 for _ in grp_before]
             new_group_now = [-1 for _ in grp_now]
             new_group_after = [-1 for _ in grp_after]
 
-            last_color = -1
+            first_color_before = -1
+            last_color_before = -1
+            first_color_now = -1
+            last_color_now = -1
+            first_color_after = -1
+            last_color_after = -1
+
             for c in grp_before:
-                if c in [grp_now] and last_color == -1:
-                    last_color = c
+                if c in grp_now and last_color_before == -1:
+                    last_color_before = c
 
+            #problem: wenn before und after gleiches hätten
+            # problem wenn nur ein element/grupppe drinnen
+            for c in grp_now:
+                if c in grp_before and first_color_now == -1:
+                    first_color_now = c
+                if c in grp_after and last_color_now == -1:
+                    last_color_now = c
 
+            for c in grp_after:
+                if c in grp_now and first_color_after == -1:
+                    first_color_after = c
 
+            new_group_before = __merge(grp_before, new_group_before, first_color_before, last_color_before)
+            new_group_now = __merge(grp_now, new_group_now, first_color_now, last_color_now)
+            new_group_after = __merge(grp_after, new_group_after, first_color_after, last_color_after)
 
+            temp_colors.extend(new_group_before)
+            if (grp_idx+2) == len(groups):
+                temp_colors.extend(new_group_now)
+                temp_colors.extend(new_group_after)
 
-    return solution
+            grp_idx += 1
+
+        new_solutions.append(RoundSolution(temp_colors))
+
+    return PSCP_Solution(new_solutions)
+
+def __merge(orig_cols, new_cols, first, last):
+    idx = 0
+    for col in [x for x in orig_cols if x == first]:
+        new_cols[idx] = col
+        idx += 1
+        orig_cols.remove(col)
+
+    idx = len(new_cols) - 1
+    for col in [x for x in orig_cols if x == last]:
+        new_cols[idx] = col
+        idx -= 1
+        orig_cols.remove(col)
+
+    idx = 0
+    for indx in range(len(new_cols)):
+        if new_cols[indx] == -1:
+            new_cols[indx] = orig_cols[idx]
+            idx += 1
+
+    return new_cols
