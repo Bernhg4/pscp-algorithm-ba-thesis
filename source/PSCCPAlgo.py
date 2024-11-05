2a3b661ff7eeb54c5a5abf7fd52075f0949570bb
Bernhard <bernhard.siegl04@gmail.com>
2024-11-05 15:05:56 +0100

    Testing and Refactoring of solutions

--- source/PSCCPAlgo.py
+++ source/PSCCPAlgo.py
@@ -3,7 +3,8 @@ import sys
 
 from source.jsonIO.json_rw import solution_to_json, instance_from_json, solution_from_json, load_json_file, \
     write_json_file
-from source.solvers.solutionGenerators import random_solution, demands_first, demands_reverse, primitive_solution
+from source.solvers.solutionGenerators import random_solution, demands_first, demands_reverse,  \
+    heuristic_solution
 from source.validator.ownSolutionValidator import validate
 from test.data_generators.test_instance_generator import gen_tiny_instance1, gen_small_instance1, gen_medium_instance1, \
     gen_big_instance1, gen_tiny_instance2, gen_small_instance2, gen_medium_instance2, gen_big_instance2
@@ -12,8 +13,9 @@ from test.data_generators.test_solution_generator import gen_tiny_solution1, gen
 
 if __name__ == "__main__":
 
-    instance_file = '../data/PSCCP_Instance7.json'
+    instance_file = '../data/PSCCP_Instance3.json'
     #instance_file = '../data/ToyInstance_ok.json'
+    time_limit_seconds = 60
 
     if len(sys.argv) == 3:
         instance_file = open(sys.argv[1])
@@ -28,13 +30,14 @@ if __name__ == "__main__":
     #printing the model like in the problem description
     print(toy_instance)
 
-    random_sol = random_solution(toy_instance)
-    demand_sol = demands_first(toy_instance)
-    reverse_sol = demands_reverse(toy_instance)
+    random_sol = random_solution(toy_instance,time_limit_seconds)
+    demand_sol = demands_first(toy_instance,time_limit_seconds)
+    reverse_sol = demands_reverse(toy_instance,time_limit_seconds)
 
-    demand_old_sol = demands_first(toy_instance,True)
-    demand_old_prior_sol = demands_first(toy_instance,True, True)
-    demand_old_prior_due_sol = demands_first(toy_instance,True, True,True)
+    demand_old_sol = demands_first(toy_instance,time_limit_seconds,True)
+    demand_old_prior_sol = demands_first(toy_instance,time_limit_seconds,True, True)
+    demand_old_prior_due_sol = demands_first(toy_instance,time_limit_seconds,True, True,True)
+    heuristic_sol = heuristic_solution(toy_instance,time_limit_seconds,10)
 
     write_json_file(solution_to_json(random_sol),instance_file.replace('.json','_random_sol.json'))
     write_json_file(solution_to_json(demand_sol),instance_file.replace('.json','_demand_sol.json'))
@@ -42,6 +45,7 @@ if __name__ == "__main__":
     write_json_file(solution_to_json(demand_old_prior_sol),instance_file.replace('.json','_demand_old_prior_sol.json'))
     write_json_file(solution_to_json(demand_old_prior_due_sol),instance_file.replace('.json','_demand_old_prior_due_sol.json'))
     write_json_file(solution_to_json(reverse_sol),instance_file.replace('.json','_reverse_sol.json'))
+    write_json_file(solution_to_json(heuristic_sol),instance_file.replace('.json','_heuristic_sol.json'))
 
     print("Random Solution:")
     validate(toy_instance,random_sol)
@@ -60,5 +64,8 @@ if __name__ == "__main__":
 
     print("\nDemands first with old color and prioritization color & due_date solution:")
     validate(toy_instance, demand_old_prior_due_sol)
+
+    print("\nHeuristic solution:")
+    validate(toy_instance, heuristic_sol)
 
 
--- test/solvers/test_solutionImprovers.py
+++ test/solvers/test_solutionImprovers.py
@@ -3,9 +3,11 @@ from unittest import TestCase
 
 from numpy.ma.extras import average, median
 
+from source.jsonIO.json_rw import load_json_file, instance_from_json
 from source.models.baseModels import RoundSolution, PSCP_Solution, PSCP_Instance, RoundInstance, Demand
-from source.solvers.solutionGenerators import primitive_solution, random_solution, demands_first
-from source.solvers.solutionImprovers import local_reorder, local_switch, primitive_local_improver, local_switch_groups
+from source.solvers.solutionGenerators import random_solution, demands_first
+from source.solvers.solutionImprovers import local_reorder, local_switch, primitive_best_improver, local_switch_groups, \
+    primitive_first_improver, run_improver, init_logging
 from source.validator.ownSolutionValidator import validate, internal_validate
 from test.data_generators.test_instance_generator import gen_small_instance1, gen_medium_instance1, gen_tiny_instance1, \
     gen_tiny_instance2, gen_big_instance1, gen_small_instance2, gen_medium_instance2
@@ -94,7 +96,7 @@ class Test(TestCase):
         maxcount = 1000
 
         for i in range(maxcount):
-            solution = random_solution(instance)
+            solution = demands_first(instance,60)
             # solution = local_reorder(instance, solution)
             #validate(instance, solution)
 
@@ -142,19 +144,30 @@ class Test(TestCase):
         #                     actual_sol.round_solutions[ind].selected_colors)
 
     def test_primitive_local_improver_small(self):
-        instance = gen_big_instance1()
+        #instance = gen_big_instance1()
 
-        res_1 = RoundSolution([1, 1, 2, 2])
-        res_2 = RoundSolution([1, 1, 1, 2])
-        solution = PSCP_Solution([res_1, res_2])
-        solution = random_solution(instance)
+        instance_file = '../../data/instance3.json'
+        instance_data = load_json_file(instance_file)
+        instance = instance_from_json(instance_data)
+
+        time_limit = 60 * 5
+
+        solution = demands_first(instance,60)
+        solution_best = copy.deepcopy(solution)
 
         validate(instance, solution)
 
-        for i in range(1000):
-            solution = primitive_local_improver(instance, solution)
+        init_logging("testInst","first")
 
-        validate(instance, solution)
+        sol_first = run_improver(instance, solution, primitive_first_improver, time_limit)
+
+        validate(instance, sol_first)
+
+        init_logging("testInst", "best")
+
+        #sol_best = run_improver(instance, solution_best, primitive_best_improver, time_limit)
+
+        #validate(instance, sol_best)
 
         #actual_sol = local_switch(instance, solution)
         #rs_1 = RoundSolution([2, 2, 2, 1, 1, 1, 1])
